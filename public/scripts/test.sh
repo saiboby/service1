aa6dbd4eae018669af6996c20d7f3aa8856b8a70
saiboby <saibabu.tan@gmail.com>
2022-10-29 19:34:00 +0530

    Update test.sh

--- public/scripts/test.sh
+++ public/scripts/test.sh
@@ -3,6 +3,7 @@ export GIT_MERGE_AUTOEDIT=no
 apt-get install git -y
 git config --global user.name "saiboby"
 git config --global user.email "saibabu.tan@gmail.com"
+git remote add origin https://saiboby:8885936276@Sai@github.com/saiboby/mainservice.git
 cd /opt
 git clone https://github.com/saiboby/mainservice.git
 cd mainservice
