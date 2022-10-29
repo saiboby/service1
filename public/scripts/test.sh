e0e2e44c2c2967785e47e3ac497af0d2476dd9cf
saiboby <saibabu.tan@gmail.com>
2022-10-29 17:05:45 +0530

    Update test.sh

--- public/scripts/test.sh
+++ public/scripts/test.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+export GIT_MERGE_AUTOEDIT=no
 apt-get install git -y
 cd /opt
-git clone https://github.com/saiboby/myrepo.git
+git clone https://github.com/saiboby/mainservice.git
+cd mainservice
+git checkout master
+git subtree pull --prefix=demo1 servce1 master
+git push origin master
