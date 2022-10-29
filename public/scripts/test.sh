471fc87c5e8d7d06bd20a70fdb2892d2b36680df
saiboby <saibabu.tan@gmail.com>
2022-10-29 17:10:36 +0530

    Update test.sh

--- public/scripts/test.sh
+++ public/scripts/test.sh
@@ -5,5 +5,6 @@ cd /opt
 git clone https://github.com/saiboby/mainservice.git
 cd mainservice
 git checkout master
+git remote add servce1 https://github.com/saiboby/service1.git
 git subtree pull --prefix=demo1 servce1 master
 git push origin master
