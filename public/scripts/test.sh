df6a3d217ae48ac2206f9d524b1538436810b9cd
saiboby <saibabu.tan@gmail.com>
2022-10-29 19:44:09 +0530

    Update test.sh

--- public/scripts/test.sh
+++ public/scripts/test.sh
@@ -3,11 +3,12 @@ export GIT_MERGE_AUTOEDIT=no
 apt-get install git -y
 git config --global user.name "saiboby"
 git config --global user.email "saibabu.tan@gmail.com"
-git remote add origin https://saiboby:8885936276@Sai@github.com/saiboby/mainservice.git
+git config --list
 cd /opt
 git clone https://github.com/saiboby/mainservice.git
 cd mainservice
 git checkout master
-git remote add servce1 https://github.com/saiboby/service1.git
-git subtree pull --prefix=demo1 servce1 master
+git remote add origin https://saiboby:8885936276@Sai@github.com/saiboby/mainservice.git
+git remote add service1 https://github.com/saiboby/service1.git
+git subtree pull --prefix=demo1 service1 master
 git push https://github.com/saiboby/mainservice.git master
