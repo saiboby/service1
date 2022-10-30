4a0bd7a7965d62d86ad5f98023d88192eecc1cc6
saiboby <saibabu.tan@gmail.com>
2022-10-30 20:40:13 +0530

    Update test.sh

--- public/scripts/test.sh
+++ public/scripts/test.sh
@@ -8,7 +8,8 @@ cd /opt
 git clone https://github.com/saiboby/mainservice.git
 cd mainservice
 git checkout master
-git remote add origin https://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@github.com/saiboby/mainservice.git
+3git remote add origin https://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@github.com/saiboby/mainservice.git
 git remote add service1 https://github.com/saiboby/service1.git
 git subtree pull --prefix=demo1 service1 master
-git push https://github.com/saiboby/mainservice.git master
+#git push https://github.com/saiboby/mainservice.git master
+git push https://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@github.com/saiboby/mainservice.git master
